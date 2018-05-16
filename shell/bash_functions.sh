1b74b1bbe4109d99d65ce1e0807a71483ef220df
Alex Armstrong <armstro1@uci.edu>
2018-05-16 10:49:05 +0200

    Added bash functions

--- shell/bash_functions.sh
+++ shell/bash_functions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+################################################################################
+# Variable checks
 function is_true() {
     local bool=$1
     [ "$bool" == true ]
@@ -33,4 +35,46 @@ function is_dir() {
 function is_not_dir() {
     local dir=$1
     [[ ! -d $dir ]]
+}
+
+################################################################################
+# Print styles
+e_header() {
+    printf "\n${bold}${purple}==========  %s  ==========${reset}\n" "$@"
+}
+e_arrow() {
+    printf "➜ $@\n"
+}
+e_success() {
+    printf "${green}✔ %s${reset}\n" "$@"
+}
+e_error() {
+    printf "${red}✖ %s${reset}\n" "$@"
+}
+e_warning() {
+    printf "${tan}➜ %s${reset}\n" "$@"
+}
+e_underline() {
+    printf "${underline}${bold}%s${reset}\n" "$@"
+}
+e_bold() {
+    printf "${bold}%s${reset}\n" "$@"
+}
+e_note() {
+    printf "${underline}${bold}${blue}Note:${reset}  ${blue}%s${reset}\n" "$@"
+}
+
+################################################################################
+# User input
+seek_confirmation() {
+  printf "\n${bold}$@${reset}"
+  read -p " (y/n) " -n 1
+  printf "\n"
+}
+# Test whether the result of an 'ask' is a confirmation
+is_confirmed() {
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  return 0
+fi
+return 1
 }
