23fdd35cedd02cc89afe59d2ce03129e2afd5ba5
Alex Armstrong <armstro1@uci.edu>
2019-05-09 01:29:35 -0700

    Overdue commit

--- ATLAS_sw/panda_task_manager.sh
+++ ATLAS_sw/panda_task_manager.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
 
 echo "Getting status" 
-pandamon -d 2 > panda_status_tasks.txt
+#pattern="user.*n0306[a-z]/"; export GRID_USER_NAME="Alex Armstrong iii"
+pattern="group.perf-muons.muTrigNt.n010*"; export GRID_USER_NAME="perf-muons"
+pandamon -d 2 | grep -v "aborted" > panda_status_tasks.txt
 echo "Sorting"
-cat panda_status_tasks.txt | grep "done.*100%" | grep -o user.*n0234[a-z]/  > complete_tasks.txt
+cat panda_status_tasks.txt | grep "done.*100%" | grep -o $pattern  > complete_tasks.txt
 sed -i.tmp 's/\//_nt/g' complete_tasks.txt
 rm *.tmp
 cat panda_status_tasks.txt | grep -v "done.*100%"  > incomplete_tasks.txt
