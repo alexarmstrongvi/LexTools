95e29cfa45089da9882d88126f58b695e3b5d493
Alex Armstrong <armstro1@uci.edu>
2020-05-03 17:27:11 -0700

    Added colors and unit tests

--- bashTools.sh
+++ bashTools.sh
@@ -18,27 +18,53 @@ function is_empty() {
 }
 function is_not_empty() {
     local var=$1
-    [[ -n $var ]]
+    [[ -n "$var" ]]
 }
 function is_file() {
     local file=$1
-    [[ -f $file ]]
+    [[ -f "$file" ]]
 }
 function is_not_file() {
     local file=$1
-    [ ! -f $file ]
+    [[ ! -f "$file" ]]
 }
 function is_dir() {
     local dir=$1
-    [[ -d $dir ]]
+    [[ -d "$dir" ]]
 }
 function is_not_dir() {
     local dir=$1
-    [[ ! -d $dir ]]
+    [[ ! -d "$dir" ]]
 }
 
 ################################################################################
 # Print styles
+# Taken from https://natelandau.com/bash-scripting-utilities/
+bold="$(tput bold)"
+underline="$(tput sgr 0 1)"
+reset="$(tput sgr0)"
+
+red="$(tput setaf 1)"
+green="$(tput setaf 2)"
+tan="$(tput setaf 3)"
+blue="$(tput setaf 4)"
+purple="$(tput setaf 5)"
+grey="$(tput setaf 6)"
+white="$(tput setaf 7)"
+
+# Taken from https://raymii.org/s/snippets/Bash_Bits_Add_Color_Output_To_Your_Scripts.html
+boldf() { echo -e "${bold}$*${reset}"; }
+underlinef() { echo -e "${underline}$*${reset}"; }
+
+redf()    { echo -e "${red}$*${reset}"; }
+greenf()  { echo -e "${green}$*${reset}"; }
+tan()     { echo -e "${tan}$*${reset}"; }
+bluef()   { echo -e "${blue}$*${reset}"; }
+purplef() { echo -e "${purple}$*${reset}"; }
+greyf()   { echo -e "${grey}$*${reset}"; }
+whitef()  { echo -e "${white}$*${reset}"; }
+
+# Special formats
 e_header() {
     printf "\n${bold}${purple}==========  %s  ==========${reset}\n" "$@"
 }
@@ -46,7 +72,7 @@ e_arrow() {
     printf "➜ $@\n"
 }
 e_success() {
-    printf "${green}✔ %s${reset}\n" "$@"
+    echo -e "${green}✔ $*${reset}"
 }
 e_error() {
     printf "${red}✖ %s${reset}\n" "$@"
@@ -70,6 +96,7 @@ seek_confirmation() {
   printf "\n${bold}$@${reset}"
   read -p " (y/n) " -n 1
   printf "\n"
+  is_confirmed
 }
 # Test whether the result of an 'ask' is a confirmation
 is_confirmed() {
